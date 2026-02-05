54967295c2aa92d89655b8d0867feaf544d89114
Anandhbasha <anadhakm@gmail.com>
2026-02-05 21:35:00 +0530

    if

---
+++ operators.c
@@ -0,0 +1,23 @@
+// Arithmetic +,-,*,/,%
+// Relational >,<,==
+// Logical && || !
+// Assignment =
+
+
+#include <stdio.h>
+
+// int main() {
+//     int a=10,b=20;
+//     printf("Grater: %d" ,a<b);
+//     return 0;
+// }
+
+int main(){
+    int num=4;
+    if(num%2==0){
+        printf("Even Number");
+    }else{
+        printf("Odd Number");
+    }
+    return 0;
+}
