de75bbe367d1a9b77f7e3a1b8285ffd70a5fb205
Anandhbasha <anadhakm@gmail.com>
2026-02-19 19:05:45 +0530

    basic

---
+++ C++/datatypes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+int main(){
+    int a =20;
+    float b= 20.2;
+    char c = 'C';
+    double d = 99.999;
+    long e = 12454545;
+    bool working = true;
+    // string name = "Hello";
+    int x;
+    // void No return value functions
+
+    std::cout<<a <<std::endl;
+
+    std::cin>>x;
+    std::cout<<b <<std::endl;
+    std::cout<<c <<std::endl;
+    std::cout<<d <<std::endl;
+    std::cout<<e <<std::endl;
+    std::cout<<working <<std::endl;
+
+    return 0;
+}
